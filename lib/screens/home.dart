167dfc17714a043bf8386da42a74942990e9090c
nathaniel-niel <nathaniel.task@gmail.com>
2022-01-03 13:04:11 +0700

    add floating button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,10 +8,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int num = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("Home")),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Home $num"),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    num += 1;
+                  });
+                },
+                backgroundColor: Colors.red,
+                child: Icon(Icons.plus_one_rounded),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
